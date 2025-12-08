422f74bfb49ea99713e6fdbbfe2a3c06f38fdf94
Inna Makhova <i.m.makhova@gmail.com>
2025-12-08 23:36:38 +0200

    feat: improve snippet page layout, refactor snippets page, and global css

--- src/pages/SnippetsPage/SnippetsPage.tsx
+++ src/pages/SnippetsPage/SnippetsPage.tsx
@@ -4,10 +4,12 @@ import { DialogTrigger } from "react-aria-components";
 import styles from "./SnippetsPage.module.css";
 import { Button } from "../../components/Button/Button.tsx";
 import { Card } from "../../components/Card/Card.tsx";
+import { Header } from "../../components/Header/Header.tsx";
 import { Table } from "../../components/Table/Table.tsx";
 import { SearchForm } from "../../forms/SearchForm/SearchForm.tsx";
 import { CreateSnippetModal } from "../../modals/CreateSnippetModal/CreateSnippetModal.tsx";
 import type { Types } from "../../types/types.ts";
+import { useToast } from "../../components/Toast/Toast.tsx";
 
 const mockSnippets: Types[] = [
   {
@@ -36,29 +38,37 @@ int main() {
 
 export function SnippetsPage() {
   const [createSnippetModalOpen, setCreateSnippetModalOpen] = useState(false);
+  const { showToast } = useToast();
   function handleDelete(snippetId: string) {
     console.log(snippetId)
   }
 
-  return (
-    <Card>
-      <div className={styles.menu}>
-        <SearchForm onSearch={() => {}} />
-        <DialogTrigger>
-          <Button onPress={() => setCreateSnippetModalOpen(true)}>
-            Create
-          </Button>
-          {createSnippetModalOpen && (
-            <CreateSnippetModal
-              isOpen={createSnippetModalOpen}
-              onClose={() => setCreateSnippetModalOpen(false)}
-              onCreate={() => {}}
-            />
-          )}
-        </DialogTrigger>
-      </div>
+  function handleCreateSnippet(/*snippet: CreateSnippetModal*/) {
+    // TODO: call backend / mutation here
+    showToast("Snippet created successfully!");
+  }
 
-      <Table snippets={mockSnippets} onDelete={handleDelete} />
-    </Card>
+  return (
+    <>
+      <Header headerText="Code snippet vault" />
+      <Card>
+        <div className={styles.menu}>
+          <SearchForm onSearch={() => {}} />
+          <DialogTrigger>
+            <Button onPress={() => setCreateSnippetModalOpen(true)}>
+              Create
+            </Button>
+            {createSnippetModalOpen && (
+              <CreateSnippetModal
+                isOpen={createSnippetModalOpen}
+                onClose={() => setCreateSnippetModalOpen(false)}
+                onCreate={handleCreateSnippet}
+              />
+            )}
+          </DialogTrigger>
+        </div>
+        <Table snippets={mockSnippets} onDelete={handleDelete} />
+      </Card>
+    </>
   )
 }
