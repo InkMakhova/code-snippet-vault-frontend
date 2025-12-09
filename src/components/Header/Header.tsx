3e90fbd1542a6dacb981c9b65a030b62a7a8bc3e
Inna Makhova <i.m.makhova@gmail.com>
2025-12-09 18:39:24 +0200

    feat: add link to GitHub repo to Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,7 @@
+import { Link } from 'react-aria-components';
 import styles from "./Header.module.css";
+import GitHubIcon from "../../assets/github-icon.svg?react";
+import { GITHUB_REPO_URL } from "../../constants.ts";
 
 export function Header({headerText}: {headerText: string}) {
   return (
@@ -6,6 +9,15 @@ export function Header({headerText}: {headerText: string}) {
       <h1 className={styles['header-title']}>
         {headerText}
       </h1>
+      <Link
+        href={GITHUB_REPO_URL}
+        target="_blank"
+        rel="noopener noreferrer"
+        className={styles['github-link']}
+      >
+        <GitHubIcon aria-hidden />
+        Project repository on GitHub
+      </Link>
     </div>
   )
 }
